64c9658299cffec4ae2b515e1151c062aaffbfd4
Yubo Wang <71800893+y-fysiks@users.noreply.github.com>
2023-09-13 10:06:36 -0400

    removed numpy dependency

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import serial
 import time
-import numpy as np
+import math
 
 serial_port = 'COM7'
 baud_rate = 250000
@@ -16,23 +16,28 @@ dn.close()
 
 dn = open("data/datanum.txt", "w")
 dn.write(str(int(datanum) + 1))
+dn.close()
 
 
-f = open("data/data.csv" + datanum, "x")
+f = open("data/data" + datanum + ".csv", "x")
 
 try:
     while True:
         # Read data from the serial port
-        data = ser.readline().decode('utf-8').split(' ')
+        data = ser.readline().decode('utf-8').split(" ")
 
         # If data received, print it
         if len(data) == 2:
-            angle = str(float(data[1]) / 1200.0 * 2 * np.pi)
             timestamp = str(float(data[0]) / 1000.0)
-        
-            print("Timestamp: " + timestamp + " Angle: " + angle)
+            angle = str(int(data[1]) / 600.0 * 2 * math.pi)
 
-            f.write(timestamp + ", " + angle + "\n")
+        
+            print("Timestamp: " + timestamp + " Angle: " + angle * 180 / math.pi)
+
+            f.write(timestamp)
+            f.write(", ")
+            f.write(angle)
+            f.write("\n")
 
             # Give the device time to send data again
             time.sleep(0.0001)
