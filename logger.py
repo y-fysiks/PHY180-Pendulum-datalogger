3800d76fe51743d2c2234536cafd96ac6d97e01a
Yubo Wang <yubo0809@outlook.com>
2023-09-12 22:57:06 -0400

    changessss

--- logger.py
+++ logger.py
@@ -1,37 +1,40 @@
 import serial
 import time
 
-serial_port = 'COM1'
-baud_rate = 9600
+serial_port = 'COM7'
+baud_rate = 115200
 
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
 datanum = 0
 
-dn = open("datanum.txt", "r")
+dn = open("data/datanum.txt", "r")
 
-datanum = int(dn.read())
+datanum = dn.read()
 dn.close()
 
+dn = open("data/datanum.txt", "w")
+dn.write(str(int(datanum) + 1))
 
-f = open("data" + datanum, "x")
+
+f = open("data/data.csv" + datanum, "x")
 
 try:
     while True:
         # Read data from the serial port
         data = ser.readline().decode('utf-8').split(' ')
 
-        angle = data[0]
-        timestamp = data[1]
-
         # If data received, print it
-        if data:
+        if len(data) == 2:
+            angle = str(float(data[0]) / 1200.0)
+            timestamp = data[1]
+        
             print("Timestamp: " + timestamp + " Angle: " + angle)
 
             f.write(timestamp + ", " + angle + "\n")
 
             # Give the device time to send data again
-            time.sleep(0.005)
+            time.sleep(0.001)
 
 # To close the serial port gracefully, use Ctrl+C to break the loop
 except KeyboardInterrupt:
