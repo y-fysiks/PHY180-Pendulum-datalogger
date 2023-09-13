dda86061a38328606b36bb9d72cf7093b008904e
EyeanL <95993002+EyeanL@users.noreply.github.com>
2023-09-12 23:45:51 -0400

    update

--- logger.py
+++ logger.py
@@ -1,8 +1,9 @@
 import serial
 import time
+import numpy as np
 
 serial_port = 'COM7'
-baud_rate = 115200
+baud_rate = 250000
 
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
@@ -26,15 +27,15 @@ try:
 
         # If data received, print it
         if len(data) == 2:
-            angle = str(float(data[0]) / 1200.0)
-            timestamp = data[1]
+            angle = str(float(data[1]) / 1200.0 * 2 * np.pi)
+            timestamp = str(float(data[0]) / 1000.0)
         
             print("Timestamp: " + timestamp + " Angle: " + angle)
 
             f.write(timestamp + ", " + angle + "\n")
 
             # Give the device time to send data again
-            time.sleep(0.001)
+            time.sleep(0.0001)
 
 # To close the serial port gracefully, use Ctrl+C to break the loop
 except KeyboardInterrupt:
