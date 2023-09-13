473c5583d41e5b6fb5bd7ed143695d6bc34b10cc
Yubo Wang <71800893+y-fysiks@users.noreply.github.com>
2023-09-12 22:00:15 -0400

    code should be finished

--- logger.py
+++ logger.py
@@ -8,32 +8,33 @@ ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
 datanum = 0
 
-dn = open("datanum", "r")
+dn = open("datanum.txt", "r")
 
 datanum = int(dn.read())
+dn.close()
 
 
 f = open("data" + datanum, "x")
-f.close()
 
 try:
     while True:
         # Read data from the serial port
-        data = ser.readline().decode('utf-8')
+        data = ser.readline().decode('utf-8').split(' ')
 
-
-        
+        angle = data[0]
+        timestamp = data[1]
 
         # If data received, print it
         if data:
-            print("Angle: ", data)
+            print("Timestamp: " + timestamp + " Angle: " + angle)
 
-            f.write(data + "\n")
+            f.write(timestamp + ", " + angle + "\n")
 
             # Give the device time to send data again
             time.sleep(0.005)
 
 # To close the serial port gracefully, use Ctrl+C to break the loop
 except KeyboardInterrupt:
-    print("Closing the serial port.")
+    print("Closing the serial port and file.")
     ser.close()
+    f.close()
